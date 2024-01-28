2c4d1b57db07bf4169308e8f3fdbfda2cf01939b
charles <mo.charles2019@gmail.com>
2024-01-28 17:37:51 -0500

    change select to bootstrap dropdown in Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,20 +1,29 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import bootstrap from 'bootstrap';
 
 const Currency = () => {
-    const { currency } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
+    const [name, setName] = useState("$ Dollar");
     
-    const [name, setName] = useState('');
+    const submitEvent = (name) => {
+        setName(name);
+        dispatch({
+            type: "CHG_CURRENCY",
+            payload: name.split(" ")[0]
+        });
+    }
 
     return (
-        <div>
-        <label for="currencySelection">Currency</label>                     
-        <select className="custom-select" id="currencySelection" onChange={(event) => setName(event.target.value)}>
-                <option value="$" name="dollar">$ Dollar</option>
-                <option value="£" name="pound">£ Pound</option>
-                <option value="€" name="Euro">€ Euro</option>
-                <option value="₹" name="Rupee">₹ Rupee</option>
-        </select>
+        <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Currency ({name})
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" onClick={(event) => submitEvent(event.target.value)}>
+                    <li><option  className="dropdown-item" value="$ Dollar" name="dollar">$ Dollar</option></li>
+                    <li><option  className="dropdown-item" value="£ Pound" name="pound">£ Pound</option></li>
+                    <li><option  className="dropdown-item" value="€ Euro" name="Euro">€ Euro</option></li>
+            </ul>
         </div>
     );
 };
