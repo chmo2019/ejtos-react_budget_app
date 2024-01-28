93eb37dfa8c29e8c1c108b94897c4aeb14ecf613
charles <mo.charles2019@gmail.com>
2024-01-28 17:41:04 -0500

    move onclick event listener into options in Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -19,10 +19,10 @@ const Currency = () => {
             <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                 Currency ({name})
             </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" onClick={(event) => submitEvent(event.target.value)}>
-                    <li><option  className="dropdown-item" value="$ Dollar" name="dollar">$ Dollar</option></li>
-                    <li><option  className="dropdown-item" value="£ Pound" name="pound">£ Pound</option></li>
-                    <li><option  className="dropdown-item" value="€ Euro" name="Euro">€ Euro</option></li>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><option  className="dropdown-item" value="$ Dollar" name="dollar" onClick={(event) => submitEvent(event.target.value)}>$ Dollar</option></li>
+                    <li><option  className="dropdown-item" value="£ Pound" name="pound" onClick={(event) => submitEvent(event.target.value)}>£ Pound</option></li>
+                    <li><option  className="dropdown-item" value="€ Euro" name="Euro" onClick={(event) => submitEvent(event.target.value)}>€ Euro</option></li>
             </ul>
         </div>
     );
