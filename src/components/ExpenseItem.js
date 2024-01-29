ee51464a51089332ccf0a664a4693ffaa0f1311d
charles <mo.charles2019@gmail.com>
2024-01-28 22:48:00 -0500

    style buttons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
-
+import bootstrap
+ from 'bootstrap';
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
 
@@ -42,8 +43,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td className="text-center"><button className="item-button add-button" onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td className="text-center"><button className="item-button red-button" onClick={event=> decreaseAllocation(props.name)}>&ndash;</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
